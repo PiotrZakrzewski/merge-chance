5499776f9f81ea5b827364deab5563461be8367c
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-12-12 17:28:19 +0100

    Tidy up the extraction script a bit

---
+++ get_pr_data.py
@@ -0,0 +1,67 @@
+from github import Github, PaginatedList
+import os
+import sys
+
+
+TOKEN = os.getenv("GH_TOKEN")
+
+
+def main():
+    if not TOKEN:
+        print("You need to set GH_TOKEN env var")
+        sys.exit(1)
+    if len(sys.argv) < 2:
+        print(
+            "Pass github repo name as first position argument.\n 'facebook/react' for example"
+        )
+        sys.exit(1)
+
+    g = Github(TOKEN)
+    target_repo_id = sys.argv[1]
+    target_repo = g.get_repo(target_repo_id)
+    csv_data = [["state", "created_at", "merged", "merge_date", "no_comments"]]
+
+    open_prs = target_repo.get_pulls()
+    print("Will process open PRs ..")
+    open_prs_data = process_pr_data(open_prs)
+    csv_data.extend(open_prs_data)
+
+    closed_prs = target_repo.get_pulls(state="closed")
+    print("Will process closed PRs ..")
+    closed_prs_data = process_pr_data(closed_prs)
+    csv_data.extend(closed_prs_data)
+
+    csv_name = f"{target_repo_id}.csv"
+    csv_name = csv_name.replace(os.sep, "_")
+    print(f"Will write results to {csv_name}")
+    with open(csv_name, "w") as outfile:
+        lines = [",".join(row) for row in csv_data]
+        text = "\n".join(lines)
+        outfile.write(text)
+
+
+def process_pr_data(paginated_gh_result: PaginatedList):
+    total = paginated_gh_result.totalCount
+    processed_prs = 0
+    print(f"{total} PRs to process...")
+    rows = []
+    for pr in paginated_gh_result:
+        created_at = pr.created_at.timestamp() if pr.created_at else "N/A"
+        merged_at = pr.merged_at.timestamp() if pr.merged_at else "N/A"
+        row = [
+            pr.state,
+            f"{created_at}",
+            f"{pr.merged}",
+            f"{merged_at}",
+            f"{pr.comments}",
+        ]
+        rows.append(row)
+        processed_prs += 1
+        if (processed_prs % 100) == 0:
+            perc_progress = round(processed_prs / total * 100, 2)
+            print(f"Processed {perc_progress} % of PRs")
+    return rows
+
+
+if __name__ == "__main__":
+    main()
