b50a5149428e05c09ac20c29d5bae55bd8c0c198
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-12-24 09:46:54 +0100

    Add outsider scores to the batch

--- batch.sh
+++ batch.sh
@@ -1,9 +1,16 @@
 
 cat $1 | tr '\n' '\0' | xargs -0 -n1 python get_pr_gql.py
-mkdir -p "$1_results"
-mv *csv "$1_results"/
-for f in $(ls "$1_results")
+CSV_DIR="$1_prs"
+OUTSIDERS_DIR="$1_prs_outsiders"
+ALL_DIR="$1_prs_all"
+mkdir -p $CSV_DIR
+mkdir -p $OUTSIDERS_DIR
+mkdir -p $ALL_DIR
+mv *csv "$CSV_DIR"/
+for f in $(ls $CSV_DIR)
 do
-  python score.py "$1_results/$f"
+  python score.py $CSV_DIR/$f  --outsiders
+  mv *png "$OUTSIDERS_DIR/"
+  python score.py "$CSV_DIR/$f"
+  mv *png "$ALL_DIR/"
 done
-mv *png "$1_results"
